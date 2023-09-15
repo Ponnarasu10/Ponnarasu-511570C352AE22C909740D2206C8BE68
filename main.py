a60801e0d9d3faecb7ea1413bcaa0a01ee88255b
Ponnarasu10 <145093369+Ponnarasu10@users.noreply.github.com>
2023-09-15 12:05:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
 
-num = 7
 
-# check if the number is negative
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   print("The factorial of", num, "is", recur_factorial(num))
+class Player(object):
+    def __init__(self, name, age, skills, style=None):
+        self.name = name
+        self.age = age
+        self.skills = skills
+        self.style = style
+
+    def get_player(self):
+        print(self.name,self.age,self.skills,self.style)
+
+
+class Team(object):
+    def __init__(self, name):
+        self.name = name
+        self._players = []
+
+    def add_player(self, obj):
+        if isinstance(obj, Player):
+            self._players.append(obj)
+        else:
+            print("Please provide player object")
+
+    def get_players(self):
+        for player in self._players:
+            player.get_player()
+
+
+if __name__ == "__main__":
+
+    p1 = Player("Mahendra", 46, "Wicket Kipper", "Right-Hand Batsman")
+    p2 = Player("Sachin", 35, "Batsman", "Right-Hand Batsman")
+    p3 = Player("Saurabh", 44, "Batsman", "Left-Hand Batsman")
+    p4 = Player("Zahir", 38, "Bauwller", "Medium Pace Bauwller")
+    p5 = Player("Yuvraj", 43, "All rounder")
+
+    t = Team("India")
+    t.add_player(p1)
+    t.add_player(p2)
+    t.add_player(p3)
+    t.add_player(p4)
+    t.add_player(p5)
+
+    t.get_players()
